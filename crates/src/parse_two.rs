a1f4dc138bacc31965eb9713209766f04651bd6a
Luke <luke.weston@cyberus-technology.de>
2026-02-06 16:44:31 +0100

    string type

--- crates/src/parse_two.rs
+++ crates/src/parse_two.rs
@@ -8,68 +8,71 @@ use std::{collections::{BTreeMap, HashMap}, env, fs};
 
 use crate::ast::Object;
 
-#[derive(Clone, Debug)]
-pub enum Json {
-    Invalid,
-    Null,
+#[derive(Clone, Debug, PartialEq)]
+enum Value {
+    U32(u32),
+    I32(i32),
+    F32(f32),
     Bool(bool),
-    Str(String),
-    Num(f64),
-    Array(Vec<Json>),
-    Object(HashMap<String, Json>),
+    Ident(String),
+    String(String),
+    Array(Vec<Value>),
+    Object(BTreeMap<String, Value>)
 }
 
-fn parser<'a>() -> impl Parser<'a, &'a str, Json, extra::Err<Rich<'a, char>>> {
+#[derive(Clone, Debug, PartialEq)]
+enum AST {
+    DeclarationScope((String, Value))
+}
+
+fn value_parser<'a>() -> impl Parser<'a, &'a str, Value, Err<Rich<'a, char>>> {
     recursive(|value| {
-        let digits = text::digits(10).to_slice();
+        let escape = just('\\').ignore_then(choice((
+            just('\\'),
+            just('/'),
+            just('"'),
+            just('n').to('\n'),
+            just('r').to('\r'),
+            just('t').to('\t'),
+        )));
 
-        let frac = just('.').then(digits);
-
-        let exp = just('e')
-            .or(just('E'))
-            .then(one_of("+-").or_not())
-            .then(digits);
-
-        let number = just('-')
-            .or_not()
-            .then(text::int(10))
-            .then(frac.or_not())
-            .then(exp.or_not())
-            .to_slice()
-            .map(|s: &str| s.parse().unwrap())
-            .boxed();
-
-        let escape = just('\\')
-            .then(choice((
-                just('\\'),
-                just('/'),
-                just('"'),
-                just('b').to('\x08'),
-                just('f').to('\x0C'),
-                just('n').to('\n'),
-                just('r').to('\r'),
-                just('t').to('\t'),
-                just('u').ignore_then(text::digits(16).exactly(4).to_slice().validate(
-                    |digits, e, emitter| {
-                        char::from_u32(u32::from_str_radix(digits, 16).unwrap()).unwrap_or_else(
-                            || {
-                                emitter.emit(Rich::custom(e.span(), "invalid unicode character"));
-                                '\u{FFFD}' // unicode replacement character
-                            },
-                        )
-                    },
-                )),
-            )))
-            .ignored()
-            .boxed();
-
-        let string = none_of("\\\"")
-            .ignored()
+        let string_value = none_of("\\\"")
             .or(escape)
             .repeated()
-            .to_slice()
-            .map(ToString::to_string)
+            .collect::<String>()
             .delimited_by(just('"'), just('"'))
+            .map(Value::String);
+
+        let digits = text::digits(10);
+        
+        let f32 = just('-').or_not()
+            .then(text::int(10))
+            .then(just('.').then(digits.clone()))
+            .to_slice()
+            .map(|s: &str| Value::F32(s.parse().unwrap()));
+
+        let i32 = just('-')
+            .then(digits.clone())
+            .to_slice()
+            .map(|s: &str| Value::I32(s.parse().unwrap()))
+            .boxed();
+
+        let u32 = digits.to_slice().map(|s: &str| Value::U32(s.parse().unwrap()));
+
+        let ident_raw = text::ascii::ident().map(ToString::to_string);
+        let ident_value = ident_raw.clone().map(|s| match s.as_str() {
+            "true" => Value::Bool(true),
+            "false" => Value::Bool(false),
+            _ => Value::Ident(s),
+        });
+
+        let kv = ident_raw.then_ignore(just(':').padded()).then(value.clone());
+        let object = kv
+            .separated_by(just(',').padded())
+            .allow_trailing()
+            .collect::<BTreeMap<String, Value>>()
+            .delimited_by(just('{').padded(), just('}').padded())
+            .map(Value::Object)
             .boxed();
 
         let array = value
@@ -88,140 +91,17 @@ fn parser<'a>() -> impl Parser<'a, &'a str, Json, extra::Err<Rich<'a, char>>> {
                     .recover_with(via_parser(end()))
                     .recover_with(skip_then_retry_until(any().ignored(), end())),
             )
-            .boxed();
-
-        let member = string.clone().then_ignore(just(':').padded()).then(value);
-        let object = member
-            .clone()
-            .separated_by(just(',').padded().recover_with(skip_then_retry_until(
-                any().ignored(),
-                one_of(",}").ignored(),
-            )))
-            .collect()
-            .padded()
-            .delimited_by(
-                just('{'),
-                just('}')
-                    .ignored()
-                    .recover_with(via_parser(end()))
-                    .recover_with(skip_then_retry_until(any().ignored(), end())),
-            )
+            .map(Value::Array)
             .boxed();
 
         choice((
-            just("null").to(Json::Null),
-            just("true").to(Json::Bool(true)),
-            just("false").to(Json::Bool(false)),
-            number.map(Json::Num),
-            string.map(Json::Str),
-            array.map(Json::Array),
-            object.map(Json::Object),
-        ))
-        .recover_with(via_parser(nested_delimiters(
-            '{',
-            '}',
-            [('[', ']')],
-            |_| Json::Invalid,
-        )))
-        .recover_with(via_parser(nested_delimiters(
-            '[',
-            ']',
-            [('{', '}')],
-            |_| Json::Invalid,
-        )))
-        .recover_with(skip_then_retry_until(
-            any().ignored(),
-            one_of(",]}").ignored(),
-        ))
-        .padded()
-    })
-}
-
-
-#[derive(Clone, Debug, PartialEq)]
-enum Value {
-    U32(u32),
-    I32(i32),
-    F32(f32),
-    Bool(bool),
-    Ident(String),
-    Array(Vec<Value>),
-    Object(BTreeMap<String, Value>)
-}
-
-#[derive(Clone, Debug, PartialEq)]
-enum AST {
-    DeclarationScope((String, Value))
-}
-
-fn value_parser<'a>() -> impl Parser<'a, &'a str, Value, Err<Rich<'a, char>>> {
-    recursive(|value| {
-        let digits = text::digits(10).to_slice();
-
-        let frac = just('.').then(digits);
-
-        let exp = just('e')
-            .or(just('E'))
-            .then(one_of("+-").or_not())
-            .then(digits);
-
-        let f32 = just('-')
-            .or_not()
-            .then(text::int(10))
-            .then(frac)
-            .then(exp.or_not())
-            .to_slice()
-            .map(|s: &str| s.parse().unwrap())
-            .boxed();
-
-        let i32 = just('-').then(digits).to_slice().map(|s: &str| s.parse().unwrap()).boxed();
-
-        let u32 = digits.to_slice().map(|s: &str| s.parse().unwrap()).boxed();
-
-        let ident_raw = text::ascii::ident().map(ToString::to_string);
-
-        let ident_value = ident_raw.clone().map(|s| match s.as_str() {
-            "true" => Value::Bool(true),
-            "false" => Value::Bool(false),
-            _ => Value::Ident(s),
-        });
-
-        let array = value
-                .clone()
-                .separated_by(just(',').padded().recover_with(skip_then_retry_until(
-                    any().ignored(),
-                    one_of(",]").ignored(),
-                )))
-                .allow_trailing()
-                .collect()
-                .padded()
-                .delimited_by(
-                    just('['),
-                    just(']')
-                        .ignored()
-                        .recover_with(via_parser(end()))
-                        .recover_with(skip_then_retry_until(any().ignored(), end())),
-                )
-                .boxed();
-
-        let kv = ident_raw
-            .then_ignore(just(':').padded())
-            .then(value);
-                
-        let object = kv
-            .separated_by(just(',').padded())
-            .collect::<BTreeMap<String, Value>>()
-            .padded()
-            .delimited_by(just('{'), just('}'))
-            .boxed();        
-
-        choice((
-            f32.map(Value::F32),
-            i32.map(Value::I32),
-            u32.map(Value::U32),
-            array.map(Value::Array),
-            object.map(Value::Object),
-            ident_value
+            f32,
+            i32,
+            u32,
+            string_value, 
+            object,
+            array,
+            ident_value,
         )).padded().boxed()
     })
 }
@@ -229,11 +109,9 @@ fn value_parser<'a>() -> impl Parser<'a, &'a str, Value, Err<Rich<'a, char>>> {
 fn ast_parser<'a>() -> impl Parser<'a, &'a str, Vec<AST>, Err<Rich<'a, char>>> {
     let ident = text::ascii::ident().map(ToString::to_string);
 
-    // This matches: node_type : alias { params } | pipe()
-    // For now, let's focus on the DeclarationScope (String, Value) logic
     let scope_block = ident
         .then_ignore(just('{').padded())
-        .then(value_parser()) // Using your existing value_parser
+        .then(value_parser())
         .then_ignore(just('}').padded())
         .map(|(name, val)| AST::DeclarationScope((name, val)));
 
@@ -247,6 +125,74 @@ fn ast_parser<'a>() -> impl Parser<'a, &'a str, Vec<AST>, Err<Rich<'a, char>>> {
 #[cfg(test)]
 mod test_two {
     use super::*;
+    use ariadne::{Color, Label, Report, ReportKind, Source};
+    use std::collections::BTreeMap;
+
+    fn assert_parse_equals_value(input: &str, expected: Value) {
+        let parser = value_parser();
+        match parser.parse(input).into_result() {
+            Ok(output) => assert_eq!(output, expected, "Parsed result didn't match expectation"),
+            Err(errors) => {
+                let errors_pretty = errors.into_iter().for_each(|e| {
+                    Report::build(ReportKind::Error, ((), e.span().into_range()))
+                        .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+                        .with_message(e.to_string())
+                        .with_label(
+                            Label::new(((), e.span().into_range()))
+                                .with_message(e.reason().to_string())
+                                .with_color(Color::Red),
+                        )
+                        .finish()
+                        .print(Source::from(&input))
+                        .unwrap()
+                });
+                println!("{:?}", errors_pretty);
+                panic!("Parse failed (see report above)");
+            }
+        }
+    }
+
+    fn assert_parse_equals_ast(input: &str, expected: Vec<AST>) {
+        let parser = ast_parser();
+        match parser.parse(input).into_result() {
+            Ok(output) => assert_eq!(output, expected, "Parsed result didn't match expectation"),
+            Err(errors) => {
+                let errors_pretty = errors.into_iter().for_each(|e| {
+                    Report::build(ReportKind::Error, ((), e.span().into_range()))
+                        .with_config(ariadne::Config::new().with_index_type(ariadne::IndexType::Byte))
+                        .with_message(e.to_string())
+                        .with_label(
+                            Label::new(((), e.span().into_range()))
+                                .with_message(e.reason().to_string())
+                                .with_color(Color::Red),
+                        )
+                        .finish()
+                        .print(Source::from(&input))
+                        .unwrap()
+                });
+                println!("{:?}", errors_pretty);
+                panic!("Parse failed (see report above)");
+            }
+        }
+    }
+
+    #[test]
+    fn test_debug_values() {
+        // Test the string specifically
+        assert_parse_equals_value(
+            r#""Line\nNew""#, 
+            Value::String("Line\nNew".to_string())
+        );
+
+        // Test the number specifically
+        assert_parse_equals_value("32", Value::U32(32));
+        
+        // Test the object
+        let input = r#"{ bio: "Rust\nRules" }"#;
+        let mut map = BTreeMap::new();
+        map.insert("bio".to_string(), Value::String("Rust\nRules".to_string()));
+        assert_parse_equals_value(input, Value::Object(map));
+    }
 
     #[test]
     fn parse_values() {
@@ -273,15 +219,29 @@ mod test_two {
         ])))
         ];
 
-        for (input, expected) in cases {
-            let result = value_parser().parse(input).into_result();
-            
-            assert_eq!(
-                result.unwrap(), 
-                expected, 
-                "Failed to parse input: '{}'", input
-            );
+        for (input, expected) in cases {            
+            assert_parse_equals_value(input, expected);
         }
+    }
+
+    #[test]
+    fn test_bogus(){
+        // Should fail as we need keyword or closing string
+        let missing_quote = r#"
+            MyScope {
+                {
+                    version: 1,
+                    settings: { 
+                        enabled: "true 
+                    }
+                }
+            }
+        "#;
+
+        let parser = value_parser();
+        let res = parser.parse(&missing_quote);
+        
+        assert_eq!(res.output().is_none(), true);
     }
 
     #[test]
@@ -297,8 +257,6 @@ mod test_two {
             }
         "#;
 
-        let result = ast_parser().parse(src.trim()).into_result();
-
         let expected_map = BTreeMap::from([
             ("version".to_string(), Value::U32(1)),
             ("settings".to_string(), Value::Object(BTreeMap::from([
@@ -313,7 +271,23 @@ mod test_two {
             ))
         ];
 
-        assert_eq!(result.unwrap(), expected_ast);
+        assert_parse_equals_ast(src, expected_ast);
+    }
+
+    #[test]
+    fn test_ident_keys_string_values() {
+        let input = r#"{ 
+            username: "Alice",
+            bio: "Software Engineer\nRust Enthusiast",
+            lucky_number: 7
+        }"#;
+
+        let mut expected_map = BTreeMap::new();
+        expected_map.insert("username".to_string(), Value::String("Alice".to_string()));
+        expected_map.insert("bio".to_string(), Value::String("Software Engineer\nRust Enthusiast".to_string()));
+        expected_map.insert("lucky_number".to_string(), Value::U32(7));
+
+        assert_parse_equals_value(input, Value::Object(expected_map));
     }
 
 }
