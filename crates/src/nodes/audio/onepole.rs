f66a83cc0fb346f591cb675c49ea6f133119115f
Luke <luke.weston@cyberus-technology.de>
2026-03-13 15:59:58 +0100

    clippy lint fix

--- crates/src/nodes/audio/onepole.rs
+++ crates/src/nodes/audio/onepole.rs
@@ -49,7 +49,7 @@ impl Node for OnePole {
     }
 
     fn handle_msg(&mut self, msg: NodeMessage) {
-        if let NodeMessage::SetParam(inner) = msg { if let ("a", RtValue::F32(val)) = (inner.param_name, inner.value) { self.a = val } }
+        if let NodeMessage::SetParam(inner) = msg && let ("a", RtValue::F32(val)) = (inner.param_name, inner.value) { self.a = val }
     }
 
     fn ports(&self) -> &Ports {
