ebbb7591cc4b3c512b36d2c947f2ad63bf55fa42
Luke <luke.weston@cyberus-technology.de>
2026-03-13 15:40:03 +0100

    attempt at CI setup

--- flake.nix
+++ flake.nix
@@ -3,116 +3,104 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    # Python config
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     uv2nix = {
       url = "github:pyproject-nix/uv2nix";
       inputs.pyproject-nix.follows = "pyproject-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     pyproject-build-systems = {
       url = "github:pyproject-nix/build-system-pkgs";
       inputs.pyproject-nix.follows = "pyproject-nix";
       inputs.uv2nix.follows = "uv2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # Rust nightly overlay
     naersk.url = "github:nix-community/naersk";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = inputs@{ self, nixpkgs, uv2nix, pyproject-nix, pyproject-build-systems, flake-parts, naersk, rust-overlay, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
-
-      perSystem = { config, pkgs, lib, system, ... }: let
-        # apply rust overlay
-        overlays = [ (import rust-overlay) ];
-        pkgs' = import nixpkgs { inherit system overlays; };
-
-        # nightly toolchain
-        nightly = pkgs'.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-          extensions = [ "rust-src" "clippy" ];
-        });
-
-        naersk' = pkgs'.callPackage naersk { };
-
-        uvWorkspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./scripts; };
-        overlay = uvWorkspace.mkPyprojectOverlay { sourcePreference = "wheel"; };
-
-        pyprojectOverrides = _final: _prev: { };
-
-        python = pkgs'.python313;
-        pythonSet =
-          (pkgs'.callPackage pyproject-nix.build.packages { inherit python; })
-          .overrideScope (lib.composeManyExtensions [
-            pyproject-build-systems.overlays.default
-            overlay
-            pyprojectOverrides
-          ]);
-
-        venv = pythonSet.mkVirtualEnv "development-scripts-default-env" uvWorkspace.deps.default;
-
-      in {
-        devShells.default = pkgs'.mkShell {
+  outputs = { self, nixpkgs, uv2nix, pyproject-nix, pyproject-build-systems, naersk, rust-overlay, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];      
+      forEachSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        inherit system;
+      });
+    in
+    {
+      devShells = forEachSystem ({ pkgs, ... }: {
+        default = let
+          nightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            extensions = [ "rust-src" "clippy" ];
+          });
+          
+          uvWorkspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./scripts; };
+          pythonSet = (pkgs.callPackage pyproject-nix.build.packages { python = pkgs.python313; })
+            .overrideScope (nixpkgs.lib.composeManyExtensions [
+              pyproject-build-systems.overlays.default
+              (uvWorkspace.mkPyprojectOverlay { sourcePreference = "wheel"; })
+            ]);
+          venv = pythonSet.mkVirtualEnv "dev-scripts-env" uvWorkspace.deps.default;
+        in
+        pkgs.mkShell {
+          # For local shell, use native cpu
           RUSTFLAGS = "-C target-cpu=native";
-          buildInputs = with pkgs'; [
-            uv
-            # rust nightly
-            nightly
-            cargo
-            rustc
-            rustfmt
+          
+          nativeBuildInputs = with pkgs; [ 
+            clang 
+            pkg-config 
+          ];
+          
+          buildInputs = with pkgs; [
+            uv 
+            nightly 
+            cargo 
+            rustc 
+            rustfmt 
             rustPackages.clippy
             # audio stack
-            alsa-lib
-            jack2
+            alsa-lib 
+            jack2 
             ffmpeg_6-full
             # misc
-            pre-commit
-            nodejs
-            pnpm
+            pre-commit 
+            nodejs 
+            pnpm 
+            venv
           ];
-
-          nativeBuildInputs = with pkgs'; [
-            clang
-            pkg-config
-          ];
-
-          packages = [ venv ];
         };
+      });
 
-        packages = {
-          rust-nightly-build = naersk'.buildPackage {
-          RUSTFLAGS = "-C target-cpu=native";
+      packages = forEachSystem ({ pkgs, system }: 
+        let
+          nightly = pkgs.rust-bin.selectLatestNightlyWith (t: t.default);
+          naersk' = pkgs.callPackage naersk { };
+          platformFlags = if pkgs.stdenv.isx86_64 then "-C target-cpu=x86-64-v3" else "";
+        in {
+          default = naersk'.buildPackage {
             src = ./.;
             nativeBuildInputs = [ nightly ];
+            RUSTFLAGS = platformFlags;
           };
-        };
+      });
 
-        apps = {
-          spectrogram = {
+      apps = forEachSystem ({ pkgs, ... }: 
+        let
+          mkApp = name: scriptPath: {
             type = "app";
-            program = pkgs'.writeShellScriptBin "spectrogram" ''
-              ${venv}/bin/python ${./scripts/dsp/spectrogram.py} "$@"
-            '';
+            program = "${pkgs.writeShellScriptBin name ''
+              python ${scriptPath} "$@"
+            ''}/bin/${name}";
           };
-
-          filter-design = {
-            type = "app";
-            program = pkgs'.writeShellScriptBin "filter-design" ''
-              ${venv}/bin/python ${./scripts/dsp/filter-design.py} "$@"
-            '';
-          };
-        };
-      };
+        in {
+          spectrogram = mkApp "spectrogram" ./scripts/dsp/spectrogram.py;
+          filter-design = mkApp "filter-design" ./scripts/dsp/filter-design.py;
+      });
     };
 }
