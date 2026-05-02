a829e27805b5ff5a50243b2b08d6a11ccc775c75
Luke <lukeweston1234@gmail.com>
2026-05-02 16:39:24 +0200

    redirect

--- docs/app/pages/index.vue
+++ docs/app/pages/index.vue
@@ -1,3 +1,5 @@
 <script setup lang="ts">
-await navigateTo("/docs/getting-started");
+definePageMeta({
+  middleware: () => navigateTo("/docs/getting-started", { redirectCode: 301 }),
+});
 </script>
