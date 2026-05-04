01297899900a415e36e59082e2254ba436da21ee
Luke <lukeweston1234@gmail.com>
2026-05-04 21:48:48 +0200

    mobile tweaks

--- docs/app/layouts/default.vue
+++ docs/app/layouts/default.vue
@@ -12,24 +12,27 @@ const links = [
     <div class="w-full h-full flex flex-col">
       <!-- Nav Bar-->
       <div
-        class="w-full max-h-24 flex justify-between items-center py-3 md:py-8 px-3 sm:px-8 md:px-16"
+        class="w-full max-h-24 flex justify-between items-center py-3 pt-4 sm:pt-3 md:py-8 px-3 sm:px-8 md:px-16"
       >
-        <div class="flex gap-3 md:gap-12 items-center">
+        <div class="flex gap-6 md:gap-12 items-center">
           <NuxtLink class="shrink-0" to="/">
-            <NuxtImg class="xl:w-36 w-24 shrink-0" src="/images/legato.svg" />
+            <NuxtImg
+              class="md:w-36 sm:w-32 w-24 shrink-0 sm:mr-3"
+              src="/images/legato.svg"
+            />
           </NuxtLink>
           <NuxtLink
             v-for="item in links"
             :key="item.to"
             :to="item.to"
             activeClass="!opacity-100"
-            class="transition-opacity opacity-60 text-base"
+            class="transition-opacity opacity-60 text-sm sm:text-base"
           >
             {{ item.title }}
           </NuxtLink>
         </div>
         <a href="https://github.com/legato-dsp">
-          <img class="w-8" src="/images/github-mark-white.svg" />
+          <img class="w-6 sm:w-8" src="/images/github-mark-white.svg" />
         </a>
       </div>
       <!-- Content -->
